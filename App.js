8ed5ddb858ff580cf083eb19e23cbf00279ca75d
Diego González Suárez <UO276406@uniovi.es>
2024-06-01 13:08:42 +0200

    Priemra versión decente

--- App.js
+++ App.js
@@ -1,181 +1,127 @@
-import { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
-  SafeAreaView,
   StyleSheet,
-  Text,
   View,
-  StatusBar,
-  Vibration,
+  Dimensions,
+  Text,
+  Animated,
+  Alert,
 } from "react-native";
-import { useKeepAwake } from "expo-keep-awake";
-import Row from "./components/Row";
-import Button from "./components/Button";
-import calculate, {
-  initialState,
-  showNumber,
-  showOperation,
-} from "./utils/calculator";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+
+import { getRandomColor } from "./utils/utils";
+
+const { width, height } = Dimensions.get("window");
 
 const App = () => {
-  useKeepAwake();
-  const [progMode, setProgMode] = useState(false);
-  const [state, setState] = useState(initialState);
-  const [history, setHistory] = useState([]);
+  const [fingers, setFingers] = useState([]);
+  const [selectedFinger, setSelectedFinger] = useState(null);
+  const animationValue = useRef(new Animated.Value(1)).current;
 
-  const addHistoryElement = (element) => {
-    setHistory([...history, element]);
+  const handleBeganGestureEvent = (event) => {
+    const { x, y } = event.nativeEvent;
+    const numberOfPointers = fingers.length + 1;
+
+    setFingers((prevFingers) => {
+      const updatedFingers = [
+        ...prevFingers,
+        { key: numberOfPointers, x, y, color: getRandomColor() },
+      ];
+      console.log("updatedFingers", updatedFingers);
+      return updatedFingers;
+    });
   };
 
-  const buttonPressed = (operation, value) => {
-    setState(calculate(operation, value, state, onError, addHistoryElement));
-  };
+  useEffect(() => {
+    let timeoutId;
 
-  const onError = () => {
-    Vibration.vibrate([400]);
-  };
+    if (fingers.length > 0) {
+      timeoutId = setTimeout(() => {
+        const randomIndex = Math.floor(Math.random() * fingers.length);
+        setSelectedFinger(fingers[randomIndex]);
+
+        Animated.sequence([
+          Animated.timing(animationValue, {
+            toValue: 2,
+            duration: 2500,
+            useNativeDriver: true,
+          }),
+          Animated.timing(animationValue, {
+            toValue: 1,
+            duration: 2500,
+            useNativeDriver: true,
+          }),
+        ]).start(() => {
+          setFingers([]);
+        });
+      }, 5000);
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [fingers]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.blockMenu}>
-        <Row>
-          <Button
-            text="Prog"
-            theme="menu"
-            onPress={() => setProgMode(!progMode)}
-          />
-        </Row>
-      </View>
-      <View style={styles.blockOperation}>
-        <Text style={styles.blockOperationText}>{showOperation(state)}</Text>
-      </View>
-      <View style={styles.blockValue}>
-        <Text style={styles.blockValueText}>
-          {showNumber(state.currentValue)}
-        </Text>
-      </View>
-      <View style={styles.blockButtons}>
-        <Row>
-          <Button
-            text="percent"
-            theme="secondary"
-            onPress={() => buttonPressed("percentage")}
-          />
-          <Button
-            text="CE"
-            theme="removal"
-            onPress={() => buttonPressed("clearLast")}
-          />
-          <Button
-            text="C"
-            theme="removal"
-            onPress={() => buttonPressed("clear")}
-          />
-          <Button
-            text="delete"
-            theme="removal"
-            onPress={() => buttonPressed("delete")}
-          />
-        </Row>
-        <Row>
-          <Button
-            text="math-log"
-            theme="secondary"
-            onPress={() => buttonPressed("log")}
-          />
-          <Button
-            text="math-sin"
-            theme="secondary"
-            onPress={() => buttonPressed("sin")}
-          />
-          <Button
-            text="square-root"
-            theme="secondary"
-            onPress={() => buttonPressed("square-root")}
-          />
-          <Button
-            text="division"
-            theme="operation"
-            onPress={() => buttonPressed("operator", "/")}
-          />
-        </Row>
-        <Row>
-          <Button text="7" onPress={() => buttonPressed("number", 7)} />
-          <Button text="8" onPress={() => buttonPressed("number", 8)} />
-          <Button text="9" onPress={() => buttonPressed("number", 9)} />
-          <Button
-            text="X"
-            theme="operation"
-            onPress={() => buttonPressed("operator", "*")}
-          />
-        </Row>
-        <Row>
-          <Button text="4" onPress={() => buttonPressed("number", 4)} />
-          <Button text="5" onPress={() => buttonPressed("number", 5)} />
-          <Button text="6" onPress={() => buttonPressed("number", 6)} />
-          <Button
-            text="-"
-            theme="operation"
-            onPress={() => buttonPressed("operator", "-")}
-          />
-        </Row>
-        <Row>
-          <Button text="1" onPress={() => buttonPressed("number", 1)} />
-          <Button text="2" onPress={() => buttonPressed("number", 2)} />
-          <Button text="3" onPress={() => buttonPressed("number", 3)} />
-          <Button
-            text="+"
-            theme="operation"
-            onPress={() => buttonPressed("operator", "+")}
-          />
-        </Row>
-        <Row>
-          <Button text="+/-" onPress={() => buttonPressed("posneg")} />
-          <Button text="0" onPress={() => buttonPressed("number", 0)} />
-          <Button text="." onPress={() => buttonPressed("number", ".")} />
-          <Button
-            text="="
-            theme="calculation"
-            onPress={() => buttonPressed("equal")}
-          />
-        </Row>
-      </View>
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.container}>
+      <PanGestureHandler onBegan={handleBeganGestureEvent}>
+        <View style={styles.touchArea}>
+          {fingers.map((finger) => (
+            <Animated.View
+              key={finger.key}
+              style={[
+                styles.finger,
+                {
+                  top: finger.y - 25,
+                  left: finger.x - 25,
+                  backgroundColor: finger.color,
+                  transform: [
+                    {
+                      scale:
+                        selectedFinger?.key === finger.key ? animationValue : 1,
+                    },
+                  ],
+                },
+              ]}
+            >
+              <Text style={[styles.text]}>{finger.key}</Text>
+            </Animated.View>
+          ))}
+        </View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  touchArea: {
+    flex: 1,
     backgroundColor: "black",
-    paddingTop: StatusBar.currentHeight,
   },
-  blockMenu: {
-    flex: 0.75,
+  finger: {
+    position: "absolute",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
-  blockOperation: {
-    flex: 1.5,
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
-    padding: 10,
+  text: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center", // Alineación horizontal centrada
+    textAlignVertical: "center", // Alineación vertical centrada
+    fontSize: 35,
   },
-  blockOperationText: {
-    color: "#fff",
-    fontSize: 30,
-  },
-  blockValue: {
-    flex: 2,
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
-    padding: 10,
-    backgroundColor: "#001000",
-    borderRadius: 20,
-    marginBottom: 5,
-  },
-  blockValueText: {
-    color: "#fff",
-    fontSize: 55,
-  },
-  blockButtons: {
-    flex: 5.75,
+  highlighted: {
+    backgroundColor: "yellow",
   },
 });
+
 export default App;
---
+++ utils/utils.js
@@ -0,0 +1,23 @@
+export const getRandomColor = () => {
+  const minBrightness = 100; // Establecer un umbral mínimo para la luminosidad
+
+  let color = `#${Math.floor(Math.random() * 0xffffff)
+    .toString(16)
+    .padStart(6, "0")}`; // Generar un color hexadecimal aleatorio
+
+  // Convertir el color a RGB
+  const rgb = parseInt(color.substring(1), 16);
+  const r = (rgb >> 16) & 0xff;
+  const g = (rgb >> 8) & 0xff;
+  const b = rgb & 0xff;
+
+  // Calcular la luminosidad (brillo)
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+  // Si la luminosidad es menor que el umbral mínimo, generar un nuevo color
+  if (brightness < minBrightness) {
+    return getRandomColor();
+  }
+
+  return color;
+};
